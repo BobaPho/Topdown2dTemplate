dfe60b907bd7b839238a7cde18b7eddeec6ae04a
Snowshinoo <109977863+Snowshinoo@users.noreply.github.com>
2023-08-21 11:58:01 -0700

    push2

---
+++ Source/YtTut/YtTutGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YtTutGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YTTUT_API AYtTutGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
